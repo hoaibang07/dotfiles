93e06d7c1aac8fb40ba02a27834200b578fc70b1
HoaiNP <phuhoaingo@gmail.com>
2019-04-09 00:27:45 +0700

    add some methods and tools

--- src/os/install/ubuntu/utils.sh
+++ src/os/install/ubuntu/utils.sh
@@ -81,16 +81,17 @@ upgrade() {
 download_and_install() {
     declare -r URL="$2"
     declare -r PACKAGE_READABLE_NAME="$1"
-    declare -r PACKAGE_FILE_NAME="$3"
 
-    # IFS='/' # space is set as delimiter
-    # read -ra parts <<< "$URL" # str is read into an array as tokens separated by IFS
-    # let len=${#parts[@]}
-    # let lastPosition="len-1"
-    # let fileName=${parts[$lastPotition]}
+    IFS='/' # space is set as delimiter
+    read -ra parts <<< "$URL" # str is read into an array as tokens separated by IFS
+    let len=${#parts[@]}
+    let lastPosition="len-1"
+    let fileName=${parts[$lastPotition]}
 
-    execute "wget -O ~/Downloads/$PACKAGE_FILE_NAME $URL" "Download $PACKAGE_READABLE_NAME"
-    execute "sudo gdebi ~/Downloads/$PACKAGE_FILE_NAME" "$PACKAGE_READABLE_NAME"
+    execute "wget -O ~/Downloads/$fileName $URL" "Download $PACKAGE_READABLE_NAME"
+    execute "sudo gdebi --option=APT::Get::force-yes=1,APT::Get::Assume-Yes=1 -n ~/Downloads/$fileName" "Install $PACKAGE_READABLE_NAME"
+
+    
 }
 
 snap_install_package() {
