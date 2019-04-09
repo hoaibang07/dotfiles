808d6353a3ea3cedfbda37f771b05f3186754e13
tiktiktac <tiktiktac07@gmail.com>
2019-04-09 10:43:21 +0700

    changed some methods

--- src/os/install/ubuntu/utils.sh
+++ src/os/install/ubuntu/utils.sh
@@ -81,18 +81,10 @@ upgrade() {
 download_and_install() {
     declare -r URL="$2"
     declare -r PACKAGE_READABLE_NAME="$1"
-    declare -r PACKAGE_FILE_NAME="$3"
 
-    # IFS='/' # space is set as delimiter
-    # read -ra parts <<< "$URL" # str is read into an array as tokens separated by IFS
-    # let len=${#parts[@]}
-    # let lastPosition="len-1"
-    # let fileName=${parts[$lastPotition]}
-
-    execute "wget -O ~/Downloads/$PACKAGE_FILE_NAME $URL" "Download $PACKAGE_READABLE_NAME"
-    execute "sudo gdebi --option=APT::Get::force-yes=1,APT::Get::Assume-Yes=1 -n ~/Downloads/$PACKAGE_FILE_NAME" "Install $PACKAGE_READABLE_NAME"
-
-    
+    fileName="$(basename -- $URL)"
+    execute "wget -O $HOME/Downloads/dotfiles_tmp/$fileName $URL" "Downloading $PACKAGE_READABLE_NAME"
+    execute "sudo gdebi --option=APT::Get::force-yes=1,APT::Get::Assume-Yes=1 -n $HOME/Downloads/dotfiles_tmp/$fileName" "Install $PACKAGE_READABLE_NAME"
 }
 
 snap_install_package() {
