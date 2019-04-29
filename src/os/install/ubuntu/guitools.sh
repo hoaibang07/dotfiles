788b3f1b20efa39ccf528b77553547265d90892a
HoaiNP <hoaibang07@gmail.com>
2019-04-30 03:02:54 +0700

    fix some errors

--- src/os/install/ubuntu/guitools.sh
+++ src/os/install/ubuntu/guitools.sh
@@ -10,4 +10,4 @@ print_in_purple "\n   GUI config Tools\n\n"
 
 install_package "xdotool" "xdotool"
 install_package "gnome-tweak-tool" "gnome-tweak-tool"
-install_package "dconf-tools" "dconf-tools"
+install_package "dconf-editor" "ddconf-editor"
