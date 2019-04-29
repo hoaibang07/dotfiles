42e5854407789d6d38af478b4663bc0aa3888804
HoaiNP <hoaibang07@gmail.com>
2019-04-30 01:04:19 +0700

    fixed font install error

--- src/os/install/ubuntu/fonts.sh
+++ src/os/install/ubuntu/fonts.sh
@@ -11,8 +11,8 @@ print_in_purple "\n   Fonts\n\n"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 if ! package_is_installed "ttf-mscorefonts-installer"; then
-
-	execute "sudo apt-get --yes --force-yes install ttf-mscorefonts-installer" \
+	echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
+	execute "sudo apt-get install ttf-mscorefonts-installer" \
 	"Install microsoft font"
 
 fi
